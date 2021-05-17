02987069bf76d44999f0b6c90e7b27ea31ee4de0
Shubhangi <shubhangi.jagtap@btl.com>
2021-05-17 19:39:45 +0100

    Initial Project Structure

---
+++ BuisnessLogic/ProcessOrder.cs
@@ -0,0 +1,22 @@
+﻿using BuisnessLogic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuisnessLogic
+{
+	class ProcessOrder
+	{
+		Ipayment _ipayment;
+		public ProcessOrder(Ipayment ipayment)
+		{
+			_ipayment = ipayment;
+		}
+
+		public string Process()
+		{
+			return _ipayment.HandleOrders();
+
+		}
+	}
+}
