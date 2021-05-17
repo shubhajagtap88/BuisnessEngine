1564d342f71a1cbc68b1de104a5788c4122135bc
Shubhangi <shubhangi.jagtap@btl.com>
2021-05-17 23:18:20 +0100

    Added unit tests for video

---
+++ BuisnessLogic/Video.cs
@@ -0,0 +1,21 @@
+﻿using BuisnessLogic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuisnessLogic
+{
+	public class Video : Ipayment
+	{
+		public string Name { get; set; }
+		public string HandleOrders()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Boolean IsAddSafetyBook()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ UnitTests/VideoTests.cs
@@ -0,0 +1,33 @@
+﻿using BuisnessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests
+{
+	[TestClass]
+	class VideoTests
+	{
+		[TestMethod]
+		public void Video_AddSafetyBook()
+		{
+			var video = new Video();
+			video.Name = "Learning to Ski";
+
+			var result = video.IsAddSafetyBook();
+
+			Assert.AreEqual(result, true);
+		}
+
+		public void Video_NotAddSafetyBook()
+		{
+			var video = new Video();
+			video.Name = "Harr Potter";
+
+			var result = video.IsAddSafetyBook();
+
+			Assert.AreEqual(result, true);
+		}
+	}
+}
